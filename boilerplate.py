bda82927c76d840abf78bdd3752f0acad49a538a
Paul Bowen-Huggett <paulhuggett@mac.com>
2023-09-28 11:21:42 +0200

    Working through pylint warnings.

--- boilerplate.py
+++ boilerplate.py
@@ -20,7 +20,6 @@ from typing import Optional
 import argparse
 import os.path
 import re
-import subprocess
 import sys
 
 # 3rd party
@@ -29,16 +28,14 @@ import pyfiglet
 def strip_lines(lines, index, comment_str):
     """Removes leading and trailing comment lines from the file."""
 
-    is_blank = lambda x: x == '' or x == '\n'
-
     # Remove blank lines
-    while len(lines) > 0 and is_blank(lines[index]):
+    while len(lines) > 0 and (lines[index] == '' or lines[index] == '\n'):
         del lines[index]
 
     # Remove comment lines.
     stop_str = comment_str + comment_str[0]
-    is_deletable = lambda x: x.startswith(comment_str)
-    while len(lines) > 0 and is_deletable(lines[index]) and not lines[index].startswith(stop_str):
+    while (len(lines) > 0 and lines[index].startswith(comment_str) and
+          not lines[index].startswith(stop_str)):
         del lines[index]
 
     return lines
@@ -58,21 +55,27 @@ def strip_leading_and_trailing_lines(lines, comment):
 
 
 def remove_string_prefix(s:str, prefix:str) -> str:
-    if s.startswith(prefix):
-        s = s[len(prefix):]
-    return s
+    """
+    If the string s starts with the supplied prefix, it is removed otherwise the string is
+    returned unaltered.
+    """
+
+    return s[len(prefix):] if s.startswith(prefix) else s
 
 
 def remove_string_suffix(s:str, suffix:str) -> str:
-    if s.endswith(suffix):
-        s = s[:-len(suffix)]
-    return s
+    """
+    If the string s ends with the supplied suffix, it is removed otherwise the string is
+    returned unaltered.
+    """
+
+    return s[:-len(suffix)] if s.endswith(suffix) else s
 
 
 def split_extension(path:str) -> tuple[str,str]:
-    """A wrapper around splitext() which will delete a '.in' extension. This convention is used for files
-    being fed as the template file to cmake's configure_file() command; the resulting file has the same
-    name but without the .in part."""
+    """A wrapper around splitext() which will delete a '.in' extension. This convention is used for
+    files being fed as the template file to cmake's configure_file() command; the resulting file
+    has the same name but without the .in part."""
 
     result = os.path.splitext(path)
     if result[1] == '.in':
@@ -81,14 +84,19 @@ def split_extension(path:str) -> tuple[str,str]:
 
 
 def tu_name_from_path(path:str) -> str:
-    bn = os.path.basename(path)
-    name = split_extension(bn)[0]
+    """
+    Tries to extract the name of the translation unit from a path string. Common prefixes and
+    suffixes are removed and snake-/camel-case word splitting is turned into spaces.
+    """
+
+    base_name = os.path.basename(path)
+    name = split_extension(base_name)[0]
     name = remove_string_prefix(name, 'test_') # snake_case
     name = remove_string_prefix(name, 'Test') # CamelCase.
     name = remove_string_suffix(name, '_win32')
     name = remove_string_suffix(name, '_posix')
 
-    if bn != 'CMakeLists.txt':
+    if base_name != 'CMakeLists.txt':
         # In CamelCase, a lower-to-upper transition is a space.
         name = re.sub (r'([a-z])([A-Z])', r'\1 \2', name)
 
@@ -113,7 +121,7 @@ def get_path_line(path:str, comment_char:str) -> list[str]:
     (_, ext) = split_extension(path)
     language = LANGUAGE_MAPPING.get (ext, '')
     if len(language) > 0:
-        path_line_suffix = '*- mode: {0} -*-'.format (language) + path_line_suffix
+        path_line_suffix = f"*- mode: {language} -*-{path_line_suffix}"
 
     path_line = comment_char + '===- ' + path + ' '
     path_line += '-' * (80 - len(path_line) - len(path_line_suffix)) + path_line_suffix
@@ -138,7 +146,7 @@ COMMENT_MAPPING = {
 }
 
 
-def boilerplate(path:str, base_path:str, license_text:list[str], 
+def boilerplate(path:str, base_path:str, license_text:list[str],
                 comment_char:Optional[str]=None, figlet_enabled:bool=True) -> list[str]:
     path = os.path.abspath(path)
     base_path = os.path.abspath(base_path)
@@ -154,17 +162,17 @@ def boilerplate(path:str, base_path:str, license_text:list[str],
         base_path += os.sep
 
     if path[:len(base_path)] != base_path:
-        raise RuntimeError('path (%s) was not inside base-path (%s)' % (path, base_path))
+        raise RuntimeError(f"path ({path}) was not inside base-path ({base_path})")
 
     subpath = path[len(base_path):]
 
-    # files that are input to CMake's configure_files() sometimes end with 
+    # files that are input to CMake's configure_files() sometimes end with
     # '.in'. Remove them.
     if subpath.endswith('.in'):
         subpath = subpath[:-len('.in')]
 
-    with open(path) as f:
-        lines = f.readlines()
+    with open(path, encoding="utf8") as source_file:
+        lines = source_file.readlines()
 
     shebang = lines[0] if len(lines) > 0 and lines[0].startswith ('#!') else None
     if shebang is not None:
@@ -180,7 +188,7 @@ def boilerplate(path:str, base_path:str, license_text:list[str],
 
     prolog += get_path_line(subpath, comment_char)
 
-    line_of_dashes = comment_char + '===----------------------------------------------------------------------===//\n'
+    line_of_dashes = comment_char + '===' + '-'*70 + '===//\n'
     if figlet_enabled:
         prolog += figlet(tu_name, comment_char)
         prolog += [ line_of_dashes ]
@@ -191,11 +199,17 @@ def boilerplate(path:str, base_path:str, license_text:list[str],
 
 
 def find_boilerplate ():
+    """
+    Searches for the .boilerplate.txt file which contains the boilerplate text to
+    be added to the head of the file. We start in the current directory and work up the
+    directory tree until reaching the root.
+    """
+
     # Start in the current directory and work up the tree from there.
     path = os.getcwd()
     while True:
         try:
-            return open(os.path.join (path, ".boilerplate.txt"))
+            return open(os.path.join (path, ".boilerplate.txt"), encoding="utf8")
         except IOError:
             # Try again with the parent directory if there is one.
             newpath = os.path.abspath (os.path.join (path, os.path.pardir))
@@ -205,23 +219,25 @@ def find_boilerplate ():
             path = newpath
 
 
-def boilerplate_out(path:str, base_path:str, comment_char:Optional[str]=None, inplace:bool=False, figlet_enabled:bool=True) -> None:
-    with find_boilerplate () as f:
-        license_text = f.readlines() if f is not None else ["license text"]
+def boilerplate_out(path:str, base_path:str, comment_char:Optional[str]=None,
+                    inplace:bool=False, figlet_enabled:bool=True) -> None:
+    with find_boilerplate () as bp_file:
+        license_text = bp_file.readlines() if bp_file is not None else ["license text"]
 
     lines = boilerplate(path, base_path, license_text, comment_char, figlet_enabled)
-    outfile = open(path, 'w') if inplace else sys.stdout
-    for l in lines:
-        print(l, end='', file=outfile)
+    outfile = open(path, mode='w', encoding='utf8') if inplace else sys.stdout
+    for line in lines:
+        print(line, end='', file=outfile)
 
 
 def main(args=sys.argv[1:]):
     parser = argparse.ArgumentParser(description='Generate source file license boilerplate')
     parser.add_argument('source_file', help='The source file to be processed')
-    parser.add_argument('--base-path', help='The base path to which path names are relative', default=os.getcwd())
+    parser.add_argument('--base-path', help='The base path to which path names are relative',
+                        default=os.getcwd())
     parser.add_argument('--comment-char', help='The character(s) used to begin a comment.')
     parser.add_argument('--no-figlet',
-                        help='Disables generation of the TU banner name (using the external "figlet" utility',
+                        help='Disables generation of the TU banner name',
                         dest='figlet_enabled', action='store_false')
     parser.add_argument('-i', dest='inplace', action='store_true', help='Inplace edit file.')
     options = parser.parse_args(args)
